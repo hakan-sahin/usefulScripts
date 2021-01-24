37dd174c8f1e30e8c04eac1c2ea75b6eb092b7b5
Hakan Sahin <hakan.s1989@gmail.com>
2021-01-24 11:51:13 +0100

    Befehle fuer die Bitlocker-Verschluesselung

---
+++ bitlocker_encryption_commands.ps1
@@ -0,0 +1,11 @@
+﻿# How to use
+# "x" = Laufwerksbuchstabe; "computername" = Computername des zu erreichenden Computers bei Remote-Befehl
+
+# zeige den Status der Bitlocker-Verschluesselung auf dem Laufwerk an
+manage-bde -status "x"
+# deaktiviere die Bitlocker-Verschluesselung auf dem Laufwerk
+manage-bde off "x"
+# starte die Bitlocker-Verschluesselung ohne Hardwaretest und fuege Wiederherstellungskennwort hinzu
+manage-bde -on "x" -skiphardwaretest -recoverypassword
+# gleicher Befehl wie oben als Remotebefehl. Nach dem -cn-Parameter kommt der Computername
+manage-bde -on "x" -skiphardwaretest -recoverypassword -cn "computername"
