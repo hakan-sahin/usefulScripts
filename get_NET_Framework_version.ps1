1930407de054f8e14d2021e00ff3cddc1e98d356
Hakan Sahin <hakan.s1989@gmail.com>
2021-06-27 12:17:16 +0200

    added: check if paths exists throught Test-Path

--- get_NET_Framework_version.ps1
+++ get_NET_Framework_version.ps1
@@ -1,2 +1,4 @@
 ﻿# Get the Version of NET-Framework on curren system
-Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Net Framework Setup\NDP\v4\Full" | Get-ItemProperty -Name Version | Select-Object -ExpandProperty Version
+if (Test-Path "HKLM:\SOFTWARE\Microsoft\Net Framework Setup\NDP\v4\Full"){
+	Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Net Framework Setup\NDP\v4\Full" | Get-ItemProperty -Name Version | Select-Object -ExpandProperty Version
+}
--- get_usb_devices.ps1
+++ get_usb_devices.ps1
@@ -1,5 +1,9 @@
 ﻿# get all usb devices which had been used on this system
 $pathToDevices = "HKLM:\SYSTEM\CurrentControlSet\Enum\USBSTOR"
+#check if path exists
+if(-not (Test-Path $pathToDevices)){
+	Write-Output "Path does not exist! Either on this system had never been used an usb story or you registry got problem..check path!"
+}
 $devices = Get-ChildItem $pathToDevices
 
 foreach($item in $devices){
