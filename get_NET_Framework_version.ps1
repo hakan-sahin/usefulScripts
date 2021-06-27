12231f7a0dc337d1f709c3ab0e6104fe68943d79
Hakan Sahin <hakan.s1989@gmail.com>
2021-06-27 12:04:07 +0200

    script for: getting NET-Framework Version on currently Windows system/ getting all used USB devices on system

---
+++ get_NET_Framework_version.ps1
@@ -0,0 +1,2 @@
+﻿# Get the Version of NET-Framework on curren system
+Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Net Framework Setup\NDP\v4\Full" | Get-ItemProperty -Name Version | Select-Object -ExpandProperty Version
---
+++ get_usb_devices.ps1
@@ -0,0 +1,9 @@
+﻿# get all usb devices which had been used on this system
+$pathToDevices = "HKLM:\SYSTEM\CurrentControlSet\Enum\USBSTOR"
+$devices = Get-ChildItem $pathToDevices
+
+foreach($item in $devices){
+    $pathString = $item.ToString()
+    $pathString = $pathString.Replace("HKEY_LOCAL_MACHINE", "HKLM:")
+    Get-ChildItem $pathString | Get-ItemProperty -Name FriendlyName | Select-Object -ExpandProperty FriendlyName
+}
