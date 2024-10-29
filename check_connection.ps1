497e457e0f1f9a1212cbb817ce58df21e3ca4c37
Hakan Sahin <44474690+hakan-sahin@users.noreply.github.com>
2024-10-29 20:13:38 +0100

    connection_test script
    using the google dns server to check internet connection

---
+++ check_connection.ps1
@@ -0,0 +1,14 @@
+﻿$currDate = Get-Date -Format "dd-MM-yyyy hh:mm:ss"
+$tmp = ""
+while($true){
+    if(-not (Test-Connection 8.8.8.8 -Count 1 -ErrorAction SilentlyContinue)){
+        Write-Host "Verbindungsabbruch! Zeit der Nichterreichbarkeit: " $currDate
+        "Verbindungsabbruch! Zeit der Nichterreichbarkeit: " + $currDate + "`n" | Out-File -FilePath "C:\tmp\connection.log" -Encoding utf8 -Append -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 1
+    }else{
+        Write-Host "Erfolgreiche Verbindung"
+        #"Erfolgreiche Verbindung: " + $currDate + "`n" | Out-File -FilePath "C:\tmp\connection.log" -Encoding utf8 -Append -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 1
+        clear
+    }
+}
